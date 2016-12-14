49c13a576c6b9d6e82892cc97596afdbd2e71c69
rarimoto <masae1263@gmail.com>
2016-12-13 19:55:58 -1000

    got toggle nav going, working on main nav hover

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
 // put all your JS in here
 $('.parallax-window').parallax({imageSrc: 'src/images/foot.png'});
+
+$(document).ready(function(){
+	$('.toggle').click(function(){
+		$(this).toggleClass('open');
+	});
+});
