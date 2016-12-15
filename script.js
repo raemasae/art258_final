073ba9f2da8e5050e7b37048a2e4ae74b6e16179
rarimoto <masae1263@gmail.com>
2016-12-14 23:55:26 -1000

    bff mugs

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // put all your JS in here
 $('.parallax-window').parallax({imageSrc: 'src/images/foot.png'});
+$('.parallax-window-bff').parallax({imageSrc: 'images/bff-hero.png'});
+$('.parallax-window-bff-girls').parallax({imageSrc: 'images/girls.jpg'});
+$('.parallax-window-bff-names').parallax({imageSrc: 'images/names.jpg'});
 
 $(document).ready(function(){
 	$('.toggle').click(function(){
